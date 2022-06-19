5152e24486ea66e5ba2da11262bbca930411bad6
toshiki-anraku <62826339+toshiki-anraku@users.noreply.github.com>
2022-06-19 15:08:59 +0900

    add Laravel-adminインストール完了

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            AdminSeeder::class,
+            AdminTablesSeeder::class,
             MstGenreSeeder::class,
             ProjectSeeder::class,
             CommentSeeder::class,
